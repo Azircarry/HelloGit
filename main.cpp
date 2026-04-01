c041e2c0112f3f9fdf139808bfe8feb6dd9a836a
Azircarry <932474071@qq.com>
2026-04-02 00:28:50 +0800

    胡汉三修改了数组  添加了冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+//胡汉三添加sort 冒泡排序算法
+void sort(int arr[], int size)
+{
+	int tmp = 0;
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if(arr[j] < arr[j+1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
 int main()
 {
-	//添加数组
-	int arr[] = {10,20,32,1,3,45,55};
+	//添加数组  胡汉三修改了数组
+	int arr[] = {10,20,32,1,3,45,55,66,33,0};
 	std::cout << "hello git" << std::endl;
-       	return 0;
+    return 0;
 }
